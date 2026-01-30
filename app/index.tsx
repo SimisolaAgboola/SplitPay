58763e259b1ffcbf96f54dd02b3548bdc8f8c25b
Simisola Agboola <simisolaagboola@Mac.ipnxng.net>
2026-01-30 21:06:44 +0100

    restaurant login screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,12 @@
-import { Text, View } from "react-native";
+import React, { useState } from 'react';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Index() {
+  const [text, setText] = useState('');
+  const [pin, setPin] = useState('');
+  const handlePress = () =>{
+    Alert.alert('Simple button')
+  }
   return (
     <View
       style={{
@@ -9,7 +15,34 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Logo</Text>
+      <Text>Staff Login</Text>
+       <TextInput
+        style={styles.input}
+        onChangeText={setText}
+        value={text}
+        placeholder="Enter restaurant code"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setText}
+        value={pin}
+        placeholder="Enter restaurant pin"
+      />
+      <Button
+        title="Login"
+        onPress={handlePress}
+        color="black" // Adjusts color (iOS text, Android background)
+        accessibilityLabel="Learn more about this button" // Recommended for accessibility
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    borderRadius: 5,
+    borderColor: 'black',
+    borderWidth: 2,
+  },
+});
