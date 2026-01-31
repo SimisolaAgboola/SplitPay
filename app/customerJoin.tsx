4926e5d7bf5c4372ab88127c2d9bbbb5f8e61faf
Simisola Agboola <simisolaagboola@Mac.ipnxng.net>
2026-01-31 12:29:46 +0100

    Customer Join page

---
+++ app/customerJoin.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
+
+export default function Index() {
+  const [text, setText] = useState('');
+  const [pin, setPin] = useState('');
+  const handlePress = () =>{
+    Alert.alert('Simple button')
+  }
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Join Bill</Text>
+      <Text>Enter your name to join this bill</Text>
+       <TextInput
+        style={styles.input}
+        onChangeText={setText}
+        value={text}
+        placeholder="First Name"
+      />
+      <Button
+        title="Continue"
+        onPress={handlePress}
+        color="black" // Adjusts color (iOS text, Android background)
+        accessibilityLabel="Learn more about this button" // Recommended for accessibility
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    borderRadius: 5,
+    borderColor: 'black',
+    borderWidth: 2,
+  },
+});
